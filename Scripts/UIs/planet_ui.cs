dc9a6fd587329e7b453ad576a8308ee0bb6395f6
Civermau <mausan.bedolla@gmail.com>
2023-05-25 19:22:27 -0600

    Create userInterafce, still not implemented

---
+++ Scripts/UIs/planet_ui.cs
@@ -0,0 +1,28 @@
+using Godot;
+using System;
+
+public partial class planet_ui : Control
+{
+	[Export(PropertyHint.Flags)]
+	private bool isMenuShown = false;
+	public override void _Ready()
+	{
+		GD.Print("UI succesfully loaded");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	private void _on_upgrades_pressed()
+	{
+		if (!isMenuShown)
+		{
+			GetNode<AnimationPlayer>("Animations").Play("ShowHide-UpgMenu");
+		} else
+		{
+            GetNode<AnimationPlayer>("Animations").PlayBackwards("ShowHide-UpgMenu");
+        }
+	}
+}
